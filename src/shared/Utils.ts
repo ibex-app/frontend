1fa7c52b3d5d5b54e4806f9ae0bf9cbc1966b3c7
givigelashvili <givi.gelashvili@ingka.com>
2022-03-23 20:07:17 +0400

    Bug when removing filter array item, empty prop

--- src/shared/Utils.ts
+++ src/shared/Utils.ts
@@ -10,15 +10,15 @@ export const getParamsAsObject = () => {
   // const { data }: { data: FilterElement[] } = require('/data/filter.json')
 
   const params = window.location.search.substring(1).split('&');
-  let paramsObject: { [key: string]: string | Array<string>} = {};
+  let paramsObject: { [key: string]: string | Array<string> } = {};
 
-  if(!params[0]) return paramsObject
+  if (!params[0]) return paramsObject
   params
     .map(key => key.split('='))
     .forEach(([key, value]) => {
-      let _value:string | Array<string> = decodeURIComponent(value)
-      
-      if(['platform', 'author_platform_id', 'topics', 'persons', 'locations'].indexOf(key) > -1){
+      let _value: string | Array<string> = decodeURIComponent(value)
+
+      if (['platform', 'author_platform_id', 'topics', 'persons', 'locations'].indexOf(key) > -1) {
         _value = _value.split(',')
       }
       paramsObject[key] = _value
@@ -30,6 +30,11 @@ export const getParamsAsObject = () => {
 
 export const addParamsToUrl = (params: { [key: string]: string }) => {
   const url = new URL(window.location.href);
-  Object.keys(params).forEach(key => url.searchParams.set(key, params[key]));
+  Object.keys(params).forEach(key =>
+    (Array.isArray(params[key]) ? params[key].length : params[key])
+    ? url.searchParams.set(key, params[key]) 
+    : url.searchParams.delete(key)
+    );
+
   window.history.replaceState({}, "", url.toString());
 }
