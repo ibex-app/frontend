86af62179753a72bf8f2c4c01c8d12c558a96a4b
davitjnz <djanezashvili@gmail.com>
2022-09-20 14:56:00 +0400

    change summary to DoughnutChart

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -14,13 +14,13 @@ export function Summary() {
         <div>
             By keywords
             {/* <LineChart axisX={keyword} axisY={count}/> */}
-            {/* <BarChart  axisX={keyword} axisY={count}/> */}
+            {/* <DoughnutChart  axisX={keyword} axisY={count}/> */}
             <button>Download</button>
         </div>
         <div>
             By Accounts
             {/* <LineChart axisX={account} axisY={count}/> */}
-            {/* <BarChart  axisX={account} axisY={count}/> */}
+            {/* <DoughnutChart  axisX={account} axisY={count}/> */}
             <button>Download</button>
         </div>
 
