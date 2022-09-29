75511ae085ce4cbd5d15c8feb534164adf2e5b68
imatiashvili <i.matiashvili@sweeftdigital.com>
2022-09-29 18:52:21 +0400

    Fix download button functionality

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -16,52 +16,73 @@ import classes from './Summary.module.css';
 // }
 
 export function Summary({ filter, axisX, axisY }: SummaryInputParams) {
+    const [fileLink, setFileLink] = useState<any>("");
     const [fileLinkByPlatforms, setFileLinkByPlatforms] = useState<any>("");
     const [fileLinkByKeywords, setFileLinkByKeywords] = useState<any>("");
     const [fileLinkByAccounts, setFileLinkByAccounts] = useState<any>("");
     
-    const [loading, setLoading] = useState(false);
-    
-    const generateLink = (fileLinkType: string) => {
+    const [platformLoading, setPlatformLoading] = useState(false);
+    const [keywordLoading, setKeywordLoading] = useState(false);
+    const [accountLoading, setAccountLoading] = useState(false);
+
+    const [platformLoadingText, setPlatformLoadingText] = useState("Download");
+    const [keywordLoadingText, setKeywordLoadingText] = useState("Download");
+    const [accountLoadingText, setAccountLoadingText] = useState("Download");
+
+
+    const getDownloadLink = (axisX: string, axisY: string) => {
         const fetchData = Get('download_posts_aggregated', {
             post_request_params: transform_filters_to_request(filter),
             axisX: axisX,
             axisY: axisY
-          });
-          
+        });
+
         fetchData.then((_data: Response<any>) => {
-            setLoading(true);
-            if (fileLinkType === "platforms") {
-                let maybeData: any = E.getOrElse(() => fileLinkByPlatforms)(_data)
-                if (!maybeData.file_location) {
-                    return
-                }
-                else {
-                    setLoading(false);
-                    setFileLinkByPlatforms(maybeData)
-                }
-            }
-            else if (fileLinkType === "keywords") {
-                let maybeData: any = E.getOrElse(() => fileLinkByKeywords)(_data)
-                if (!maybeData.file_location) {
-                    return
-                }
-                else {
-                    setLoading(false);
-                    setFileLinkByKeywords(maybeData)
-                }
-            }
-            else if (fileLinkType === "accounts"){
+            if (axisX === 'account') {
                 let maybeData: any = E.getOrElse(() => fileLinkByAccounts)(_data)
                 if (!maybeData.file_location) {
-                    return
+                    return;
                 }
                 else {
-                    setLoading(false);
-                    setFileLinkByAccounts(maybeData)
+                    setAccountLoading(false);
+                    setAccountLoadingText("Download");
+                    setFileLinkByAccounts(maybeData?.file_location);
+                    window.location.href = maybeData?.file_location;
+                }
+            }
+            else if (axisX === 'keyword') {
+                let maybeData: any = E.getOrElse(() => fileLinkByKeywords)(_data)
+                if (!maybeData.file_location) {
+                    return;
+                }
+                else {
+                    setKeywordLoading(false);
+                    setKeywordLoadingText("Download");
+                    setFileLinkByKeywords(maybeData?.file_location);
+                    window.location.href = maybeData?.file_location;
+                }
+            }
+            else if (axisX === 'platform') {
+                let maybeData: any = E.getOrElse(() => fileLinkByPlatforms)(_data)
+                if (!maybeData.file_location) {
+                    return;
+                }
+                else {
+                    setPlatformLoading(false);
+                    setPlatformLoadingText("Download");
+                    setFileLinkByPlatforms(maybeData?.file_location);
+                    window.location.href = maybeData?.file_location;
                 }
             }
         });
+    }
+
+    const generateDynamicLink = (axisX: string, axisY: string) => {
+        if (axisX === 'platform') setPlatformLoadingText("Downloading...");
+        else if (axisX === 'keyword') setKeywordLoadingText("Downloading...");
+        else if (axisX === 'account') setAccountLoadingText("Downloading...")
+        
+        getDownloadLink(axisX, axisY);
     }
 
     return <div className='results'>
@@ -74,43 +95,28 @@ export function Summary({ filter, axisX, axisY }: SummaryInputParams) {
             <div>By Platforms</div>
             <LineChart axisX='platform' axisY='count' filter={filter} />
             <DoughnatChart axisX='platform' axisY='count' filter={filter} />
-            <button onClick={() => generateLink("platforms")}>
-                {
-                    fileLinkByPlatforms ? "Download Link Generated" : "Generate Download Link"
-                }
-            </button>
-
-            {
-                fileLinkByPlatforms && !loading && <a className={classes.summaryDownloadLink} href={`${fileLinkByPlatforms?.file_location}`}>Download File</a>
-            }
+            
+            <button onClick={() => generateDynamicLink('platform', 'count')}>
+                {platformLoadingText}
+            </button>    
         </div>
         <div className='dashbord-block post'>
             <div>By keywords</div>
             <BarChart axisX='platform' axisY='count' filter={filter} />
             {/* <DoughnutChart  axisX={keyword} axisY={count}/> */}
-            <button onClick={() => generateLink("keywords")}>
-                {
-                    fileLinkByKeywords ? "Download Link Generated" : "Generate Download Link"
-                }
-            </button>
-
-            {
-                fileLinkByKeywords && !loading && <a className={classes.summaryDownloadLink} href={`${fileLinkByKeywords?.file_location}`}>Download File</a>
-            }
+            
+            <button onClick={() => generateDynamicLink('keyword', 'count')}>
+                {keywordLoadingText}
+            </button>   
         </div>
         <div className='dashbord-block post'>
             <div>By Accounts</div>
             <LineChart axisX='platform' axisY='count' filter={filter} />
             {/* <DoughnutChart  axisX={account} axisY={count}/> */}
-            <button onClick={() => generateLink("accounts")}>
-                {
-                    fileLinkByAccounts ? "Download Link Generated" : "Generate Download Link"
-                }
-            </button>
-
-            {
-                fileLinkByAccounts && !loading && <a className={classes.summaryDownloadLink} href={`${fileLinkByAccounts?.file_location}`}>Download File</a>
-            }
+           
+            <button onClick={() => generateDynamicLink('account', 'count')}>
+                {accountLoadingText}
+            </button>   
         </div>
 
         <div className='dashbord-block post'>
