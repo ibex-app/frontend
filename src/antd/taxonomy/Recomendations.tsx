582c9efc602810e9f6cf14b259b2b96cf613adb3
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2022-10-23 05:06:24 +0400

    Recommendations improved

--- src/antd/taxonomy/Recomendations.tsx
+++ src/antd/taxonomy/Recomendations.tsx
@@ -1,5 +1,5 @@
 import { Collapse, Spin } from "antd"
-import { useContext } from "react";
+import { useContext, useMemo, useState } from "react";
 import { TaxonomyContext } from "../../components/taxonomy/TaxonomyContext";
 import { drawFilterItem } from '../../shared/Utils/Taxonomy';
 import { useRecommendationsState } from '../../state/useRecommendationsState';
@@ -13,9 +13,15 @@ type Input = {
 
 export const Recommendations = ({ monitor_id, toParent }: Input) => {
   const { setUserSelection } = useContext(TaxonomyContext);
-  const { data: recommendations } = useRecommendationsState(monitor_id);
+  const { data } = useRecommendationsState(monitor_id);
+  const [added, setAdded] = useState<string[]>([]);
+
+  const recommendations = useMemo(() => data
+    ? data?.recommendations?.filter((item) => !added.includes(item.word))
+    : [], [data, added]);
 
   const AddRecommendation = (data: any) => {
+    setAdded((prev) => [...prev, data.word]);
     setUserSelection(data.word)
     // toParent(data)
   }
@@ -24,10 +30,10 @@ export const Recommendations = ({ monitor_id, toParent }: Input) => {
     {/* { recommendations.map(( rec: any) => <div>{rec}</div>) } */}
     <Panel header="Recommended keywords" key={1}>
       {/* <Table /> */}
-      {!recommendations || recommendations.is_loading
+      {!data || data.is_loading
         ? <div> Loading  <Spin></Spin></div>
-        : recommendations?.recommendations?.length
-          ? recommendations?.recommendations.map((rec: any) => <div className="recommend-row"><div className="recommend-prog"><span style={{ height: (rec.score * 100) + "%" }}></span></div>
+        : !!recommendations.length
+          ? recommendations.map((rec: any) => <div className="recommend-row" key={rec.word}><div className="recommend-prog"><span style={{ height: (rec.score * 100) + "%" }}></span></div>
             <div className="recommend-word">
               {drawFilterItem({ title: rec.word })}
             </div> <button onClick={() => AddRecommendation(rec)}>Add </button></div>)
